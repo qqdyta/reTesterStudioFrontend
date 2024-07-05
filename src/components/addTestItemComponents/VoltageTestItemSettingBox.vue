658e1d6064f06ec4a68676990201129d977dbc2b
qqdyta <qqdyta@163.com>
2024-07-05 17:08:58 +0800

    修改

--- src/components/addTestItemComponents/VoltageTestItemSettingBox.vue
+++ src/components/addTestItemComponents/VoltageTestItemSettingBox.vue
@@ -5,13 +5,14 @@ export default {
     return {
       testItemSettingData: {
         stepName: "电压测试",
+        channel: 0,
         stepType: "电压测试",
         stepIndex: 0,
         stepDescription: "电压测试描述",
         isDC: true,
-        range: 10,
-        accuracy: 0.1,
-        limitMin: 0, // 0: 3.00E-5 |  1: 2.00E-5 | 2: 1.5E-6
+        range: 0,  //         0: 0.1V | 1: 1V  | 2: 10V  | 3: 100V
+        accuracy: 0.1,  //    0: 3.00E-5 |  1: 2.00E-5 | 2: 1.5E-6
+        limitMin: 0,
         limitMax: 10,
        }
     }
@@ -20,19 +21,30 @@ export default {
 </script>
 
 <template>
-<div>
+<div class="main">
   <el-form :model="testItemSettingData" label-width="auto" style="max-width: 600px">
     <el-row>
-      <el-col :span="24">
+      <el-col :span="12">
         <el-form-item label="测试名称">
           <el-input v-model="testItemSettingData.stepName" />
         </el-form-item>
       </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="24">
-        <el-form-item label="测试类型">
-          <el-input v-model="testItemSettingData.stepType" disabled />
+      <el-col :span="12">
+        <el-form-item label="&nbsp;&nbsp;&nbsp;测试通道">
+          <el-select v-model="testItemSettingData.channel">
+            <el-option label="1" value="1"></el-option>
+            <el-option label="2" value="2"></el-option>
+            <el-option label="3" value="3"></el-option>
+            <el-option label="4" value="4"></el-option>
+            <el-option label="5" value="5"></el-option>
+            <el-option label="6" value="6"></el-option>
+            <el-option label="7" value="7"></el-option>
+            <el-option label="8" value="8"></el-option>
+            <el-option label="9" value="9"></el-option>
+            <el-option label="10" value="10"></el-option>
+            <el-option label="11" value="11"></el-option>
+            <el-option label="12" value="12"></el-option>
+          </el-select>
         </el-form-item>
       </el-col>
     </el-row>
@@ -44,9 +56,14 @@ export default {
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="24">
-        <el-form-item label="是否为直流">
-          <el-switch v-model="testItemSettingData.isDC" active-text="是直流" inactive-text="否， 是交流" />
+      <el-col :span="12">
+        <el-form-item label="类型">
+          <el-switch v-model="testItemSettingData.isDC" active-text="直流" inactive-text="交流" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="12">
+        <el-form-item label="测试类型">
+          <el-input v-model="testItemSettingData.stepType" disabled />
         </el-form-item>
       </el-col>
     </el-row>
@@ -83,10 +100,25 @@ export default {
         </el-form-item>
       </el-col>
     </el-row>
+    <div>
+      <el-row>
+        <el-col :span="16">
+
+        </el-col>
+        <el-col :span="8">
+          <div>
+            <el-button type="primary" >保存</el-button>
+            <el-button>取消</el-button>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
   </el-form>
 </div>
 </template>
 
 <style scoped>
-
+.main {
+  padding-top:30px;
+}
 </style>
--- src/components/addTestItemComponents/WaitTestItemSettingBox.vue
+++ src/components/addTestItemComponents/WaitTestItemSettingBox.vue
@@ -2,14 +2,70 @@
 export default {
   name: "WaitTestItemSettingBox",
   data() {
+    return {
+      testItemSettingData: {
+        stepName: "",
+        stepDescription: "",
+        mode: "",
+        value: "",
+        COM: ""
+      }
+    }
+  },
+  methods: {
+    formatTooltip(val) {
+      return val + '秒'
+    }
   }
 }
 </script>
 
 <template>
-<div></div>
+  <div class="main">
+    <el-form :model="testItemSettingData" label-width="auto" style="max-width: 600px">
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="测试名称">
+            <el-input v-model="testItemSettingData.stepName" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="测试描述">
+            <el-input v-model="testItemSettingData.stepDescription" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="等待时间" >
+              <el-slider v-model="testItemSettingData.value" show-input :format-tooltip="formatTooltip" :show-tooltip="true"/>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <br>
+      <div>
+        <el-row>
+          <el-col :span="16">
+
+          </el-col>
+          <el-col :span="8">
+            <div>
+              <el-button type="primary" >保存</el-button>
+              <el-button>取消</el-button>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+    </el-form>
+  </div>
+
 </template>
 
 <style scoped>
-
+.main {
+  padding-top:30px;
+  width: 80%;
+}
 </style>
