d3b43cb427a5991ae97dd41079e0bc3608f3444f
qqdyta <qqdyta@163.com>
2024-07-08 15:23:48 +0800

    新增组件

---
+++ src/components/addTestItemComponents/powerSupplyOffSettingBox.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "powerSupplyOffSettingBox",
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+
+<template>
+<div>powerSupplyOffSettingBox</div>
+</template>
+
+<style scoped>
+
+</style>
