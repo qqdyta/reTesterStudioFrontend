b122c2daaca2db6cf7abcf8d40a67857d65a3070
qqdyta <qqdyta@163.com>
2024-07-08 16:55:34 +0800

    组件完成第一版

--- src/components/addTestItemComponents/powerSupplyOnSettingBox.vue
+++ src/components/addTestItemComponents/powerSupplyOnSettingBox.vue
@@ -3,14 +3,108 @@ export default {
   name: "PowerSupplyOnSettingBox",
   data() {
     return {
-      show: false
+      testItemSettingData: {
+        stepName: "电源开启",
+        channel: 0,
+        stepType: "电源开启",
+        stepIndex: 0,
+        stepDescription: "电压测试描述",
+        isDC: true,
+        range: 0,  //         0: 0.1V | 1: 1V  | 2: 10V  | 3: 100V
+        accuracy: 0.1,  //    0: 3.00E-5 |  1: 2.00E-5 | 2: 1.5E-6
+        limitMin: 0,
+        limitMax: 10,
+        Voltage: 0,
+        CurrentLimit: 0,
+        COM: 0
+      }
     }
   }
 }
 </script>
 
 <template>
-<div>powerSupplyOnSettingBox</div>
+  <div class="main">
+    <el-form :model="testItemSettingData" label-width="auto" style="max-width: 600px">
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="测试名称">
+            <el-input v-model="testItemSettingData.stepName" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="&nbsp;&nbsp;&nbsp;电源通道">
+            <el-select v-model="testItemSettingData.channel">
+              <el-option label="1" value="1"></el-option>
+              <el-option label="2" value="2"></el-option>
+              <el-option label="3" value="3"></el-option>
+              <el-option label="4" value="4"></el-option>
+              <el-option label="5" value="5"></el-option>
+              <el-option label="6" value="6"></el-option>
+              <el-option label="7" value="7"></el-option>
+              <el-option label="8" value="8"></el-option>
+              <el-option label="9" value="9"></el-option>
+              <el-option label="10" value="10"></el-option>
+              <el-option label="11" value="11"></el-option>
+              <el-option label="12" value="12"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="测试描述">
+            <el-input v-model="testItemSettingData.stepDescription" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="测试端口">
+            <el-input v-model="testItemSettingData.COM">
+              <template #prepend>COM</template>
+            </el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="电流限制">
+            <el-switch v-model="testItemSettingData.isDC" active-text="无" inactive-text="有" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="测试类型">
+            <el-input v-model="testItemSettingData.stepType" disabled />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="电流下限">
+            <el-input-number v-model="testItemSettingData.limitMin" :disabled="testItemSettingData.isDC" controls-position="right" :min="0" :max="30" :step="0.1" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="电流上限">
+            <el-input-number v-model="testItemSettingData.limitMax" :disabled="testItemSettingData.isDC" controls-position="right" :min="0" :max="30" :step="0.1" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <div>
+        <el-row>
+          <el-col :span="16">
+          </el-col>
+          <el-col :span="8">
+            <div>
+              <el-button type="primary" >保存</el-button>
+              <el-button>取消</el-button>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+    </el-form>
+  </div>
 </template>
 
 <style scoped>
