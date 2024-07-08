7d21b7aff9ab26338a690fdd53a7b3b2b1d40be2
qqdyta <qqdyta@163.com>
2024-07-08 15:16:43 +0800

    功能完善

--- src/components/addTestItemComponents/TestItemSettingBox.vue
+++ src/components/addTestItemComponents/TestItemSettingBox.vue
@@ -1,5 +1,6 @@
 <script>
 /* eslint-disable */
+import { ref, onMounted, onUnmounted, getCurrentInstance  } from 'vue'
 import CurrentTestItemSettingBox from "@/components/addTestItemComponents/CurrentTestItemSettingBox.vue";
 import EthernetTestItemSettingBox from "@/components/addTestItemComponents/EthernetTestItemSettingBox.vue";
 import VoltageTestItemSettingBox from "@/components/addTestItemComponents/VoltageTestItemSettingBox.vue";
@@ -7,20 +8,65 @@ import SerialTestItemSettingBox from "@/components/addTestItemComponents/SerialT
 import WaitTestItemSettingBox from "@/components/addTestItemComponents/WaitTestItemSettingBox.vue";
 import LoadTestItemSettingBox from "@/components/addTestItemComponents/LoadTestItemSettingBox.vue";
 import powerSupplySettingItem from "@/components/addTestItemComponents/PowerSupplySettingItem.vue";
+import dmmSettingBox from "@/components/addTestItemComponents/dmmSettingBox.vue";
+import DmmSettingBox from "@/components/addTestItemComponents/dmmSettingBox.vue";
+import OscGetWave from "@/components/addTestItemComponents/oscGetWave.vue";
 export default {
   name: "TestItemSettingBox",
   components: {
+    DmmSettingBox,
     VoltageTestItemSettingBox,
     LoadTestItemSettingBox,
     powerSupplySettingItem,
     WaitTestItemSettingBox
+  },
+  /*
+  setup() {
+    const currentComponent = ref(null)
+    const instance = getCurrentInstance()
+    const eventBus = instance.appContext.config.globalProperties.emitter
+    const setComponent = (component) => {
+      console.log('got open setting page event', component)
+      currentComponent.value = component;
+      return {
+        'voltage': VoltageTestItemSettingBox,
+        'current': CurrentTestItemSettingBox,
+        'dmmConfig': LoadTestItemSettingBox,
+      }[component]
+
+      if(component === 'voltage'){
+        currentComponent.value = VoltageTestItemSettingBox
+      }else if(component === 'current') {
+        currentComponent.value = CurrentTestItemSettingBox
+      }else if(component === 'dmmConfig') {
+        currentComponent.value = LoadTestItemSettingBox
+      }else if(component === 'powerSupplySet') {
+        currentComponent.value = powerSupplySettingItem
+      }else if(component === 'load') {
+        currentComponent.value = LoadTestItemSettingBox
+      }else if(component === 'powerSupplySet') {
+        currentComponent.value = powerSupplySettingItem
+      }
+    }
+    onMounted(() => {
+      eventBus.on('openSettingPage', setComponent);
+    });
+
+    onUnmounted(() => {
+      eventBus.on('openSettingPage', setComponent);
+    });
+    return {
+      currentComponent,
+    }
   }
+  */
 }
 </script>
 
 <template>
 <div class="z-down-shadow box">
- <WaitTestItemSettingBox></WaitTestItemSettingBox>
+<!-- <component :is="currentComponent"></component>  -->
+<dmm-setting-box />
 </div>
 </template>
 
