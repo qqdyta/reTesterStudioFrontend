43b84bf7c5529bc7800b07f43207c1e8edc48946
qqdyta <qqdyta@163.com>
2024-07-08 15:15:51 +0800

    新增组件

---
+++ src/components/addTestItemComponents/dmmSettingBox.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'dmmSettingBox',
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+
+<template>
+<div></div>
+</template>
+
+<style scoped>
+
+</style>
