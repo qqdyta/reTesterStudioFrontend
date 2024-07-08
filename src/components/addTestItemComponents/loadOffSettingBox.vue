218a5acc4b359ed11ccee4463b558bce9ac172db
qqdyta <qqdyta@163.com>
2024-07-08 15:21:57 +0800

    新增组件

---
+++ src/components/addTestItemComponents/loadOffSettingBox.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "LoadOffSettingBox",
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+
+<template>
+<div>LoadOffSettingBox</div>
+</template>
+
+<style scoped>
+
+</style>
