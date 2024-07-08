eddee02d9d856a13de088db5952bb60837cfded2
qqdyta <qqdyta@163.com>
2024-07-08 15:16:20 +0800

    新增组件

---
+++ src/components/addTestItemComponents/oscGetWave.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "OscGetWave",
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+
+<template>
+<div></div>
+</template>
+
+<style scoped>
+
+</style>
