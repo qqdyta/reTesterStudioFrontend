64bd67fea758789d4e5f8be4de78df9bda2977e6
qqdyta <qqdyta@163.com>
2024-07-08 15:22:58 +0800

    新增组件

---
+++ src/components/addTestItemComponents/loadOnSettingBox.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'loadOnSettingBox',
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+
+<template>
+<div>loadOnSettingBox</div>
+</template>
+
+<style scoped>
+
+</style>
