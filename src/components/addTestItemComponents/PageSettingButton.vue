7e54861ba31f0fce9125dd929569fbe5492f7fa5
qqdyta <qqdyta@163.com>
2024-07-09 10:10:22 +0800

    新增组件，将测试项目设置的按钮独立

---
+++ src/components/addTestItemComponents/PageSettingButton.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: "PageSettingButton",
+  props: {
+    cardIndex: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    removeMe() {
+      console.log('removeMe', this.cardIndex)
+      this.emitter.emit('removeTestItem', this.cardIndex)
+    }
+  },
+  emits: ['click', 'removeTestItem']
+}
+
+</script>
+
+<template>
+  <div>
+    <el-row>
+      <el-col :span="16">
+        <el-button type="danger" @click="removeMe">删除此步骤</el-button>
+      </el-col>
+      <el-col :span="8">
+        <div>
+          <el-button type="primary" >保存</el-button>
+          <el-button>取消</el-button>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
