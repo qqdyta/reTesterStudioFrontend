b93aaa8b32b0a43c01317b62029f1f6907f0b844
qqdyta <qqdyta@163.com>
2024-07-09 10:08:56 +0800

    新增了监听点击按钮删除测试项目卡片

--- src/components/addTestItemComponents/loadOffSettingBox.vue
+++ src/components/addTestItemComponents/loadOffSettingBox.vue
@@ -1,16 +1,92 @@
 <script>
+import PageSettingButton from "@/components/addTestItemComponents/PageSettingButton.vue";
+
 export default {
   name: "LoadOffSettingBox",
+  components: {
+    PageSettingButton
+  },
+  props: {
+    cardIndex: {
+      type: Number,
+      default: 0
+    }
+  },
   data() {
     return {
-      show: false
+      testItemSettingData: {
+        stepName: "负载关闭",
+        channel: 0,
+        stepType: "负载关闭",
+        stepIndex: 0,
+        stepDescription: "负载关闭",
+        isDC: true,
+        range: 0,  //         0: 0.1V | 1: 1V  | 2: 10V  | 3: 100V
+        accuracy: 0.1,  //    0: 3.00E-5 |  1: 2.00E-5 | 2: 1.5E-6
+        limitMin: 0,
+        limitMax: 10,
+        Voltage: 0,
+        CurrentLimit: 0,
+        COM: 0
+      }
     }
   }
 }
 </script>
 
 <template>
-<div>LoadOffSettingBox</div>
+  <div class="main">
+    <el-form :model="testItemSettingData" label-width="auto" style="max-width: 600px">
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="步骤名称">
+            <el-input v-model="testItemSettingData.stepName" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="&nbsp;&nbsp;&nbsp;负载通道">
+            <el-select v-model="testItemSettingData.channel">
+              <el-option label="1" value="1"></el-option>
+              <el-option label="2" value="2"></el-option>
+              <el-option label="3" value="3"></el-option>
+              <el-option label="4" value="4"></el-option>
+              <el-option label="5" value="5"></el-option>
+              <el-option label="6" value="6"></el-option>
+              <el-option label="7" value="7"></el-option>
+              <el-option label="8" value="8"></el-option>
+              <el-option label="9" value="9"></el-option>
+              <el-option label="10" value="10"></el-option>
+              <el-option label="11" value="11"></el-option>
+              <el-option label="12" value="12"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="测试描述">
+            <el-input v-model="testItemSettingData.stepDescription" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <el-form-item label="测试端口">
+            <el-input v-model="testItemSettingData.COM">
+              <template #prepend>COM</template>
+            </el-input>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <div>
+        <el-row>
+          <el-col :span="24">
+            <page-setting-button :cardIndex="this.cardIndex"></page-setting-button>
+          </el-col>
+        </el-row>
+      </div>
+    </el-form>
+  </div>
 </template>
 
 <style scoped>
