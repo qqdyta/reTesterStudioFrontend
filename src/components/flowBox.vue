42d7060bec7b07bf2a3bf3a7d4b7df9fc87d7d0e
qqdyta <qqdyta@163.com>
2024-08-05 19:01:52 +0800

    新增组件

---
+++ src/components/flowBox.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: "flowBox",
+  components: {
+    PageSettingButton
+  },
+  props: {
+    cardIndex: {
+      type: Number,
+      default: 0
+    }
+  },
+  emits: ['click'],
+  data() {
+    return {
+      testItemSettingData: {
+        stepName: "",
+        stepDescription: "",
+        mode: "",
+        value: "",
+        COM: ""
+      }
+    }
+  },
+  methods: {
+    formatTooltip(val) {
+      return val + '秒'
+    }
+  }
+
+}
+</script>
+
+<template>
+<div></div>
+</template>
+
+<style scoped>
+
+</style>
