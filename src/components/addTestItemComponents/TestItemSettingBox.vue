a630ceb96b9a8abdfdca42af420c64609d40641a
qqdyta <qqdyta@163.com>
2024-08-07 15:37:42 +0800

    修改

--- src/components/addTestItemComponents/TestItemSettingBox.vue
+++ src/components/addTestItemComponents/TestItemSettingBox.vue
@@ -1,126 +1,50 @@
 <script>
-/* eslint-disable */
-import { ref, onMounted, onUnmounted, getCurrentInstance  } from 'vue'
-import CurrentTestItemSettingBox from "@/components/addTestItemComponents/testItemSettingCard/CurrentTestItemSettingBox.vue";
-import EthernetTestItemSettingBox from "@/components/addTestItemComponents/testItemSettingCard/EthernetTestItemSettingBox.vue";
-import VoltageTestItemSettingBox from "@/components/addTestItemComponents/testItemSettingCard/VoltageTestItemSettingBox.vue";
-import SerialTestItemSettingBox from "@/components/addTestItemComponents/testItemSettingCard/SerialTestItemSettingBox.vue";
-import WaitTestItemSettingBox from "@/components/addTestItemComponents/testItemSettingCard/WaitTestItemSettingBox.vue";
-import LoadTestItemSettingBox from "@/components/addTestItemComponents/testItemSettingCard/LoadTestItemSettingBox.vue";
-import powerSupplySettingItem from "@/components/addTestItemComponents/testItemSettingCard/PowerSupplySettingItem.vue";
-import dmmSettingBox from "@/components/addTestItemComponents/testItemSettingCard/dmmSettingBox.vue";
-import DmmSettingBox from "@/components/addTestItemComponents/testItemSettingCard/dmmSettingBox.vue";
-import OscGetWave from "@/components/addTestItemComponents/testItemSettingCard/oscGetWave.vue";
-import loadOnSettingBox from "@/components/addTestItemComponents/testItemSettingCard/loadOnSettingBox.vue";
-import loadOffSettingBox from "@/components/addTestItemComponents/testItemSettingCard/loadOffSettingBox.vue";
-import powerSupplyOnSettingBox from "@/components/addTestItemComponents/testItemSettingCard/powerSupplyOnSettingBox.vue";
-import powerSupplyOffSettingBox from "@/components/addTestItemComponents/testItemSettingCard/powerSupplyOffSettingBox.vue";
-
-
 export default {
-  name: "TestItemSettingBox",
-  components: {
-    DmmSettingBox,
-    VoltageTestItemSettingBox,
-    LoadTestItemSettingBox,
-    powerSupplySettingItem,
-    WaitTestItemSettingBox,
-    SerialTestItemSettingBox,
-    EthernetTestItemSettingBox,
-    CurrentTestItemSettingBox,
-    OscGetWave,
-    loadOnSettingBox,
-    loadOffSettingBox,
-    powerSupplyOnSettingBox,
-    powerSupplyOffSettingBox,
-  },
-
-  setup() {
-    const currentComponent = ref(null)
-    const currentIndex = ref(0)
-    const instance = getCurrentInstance()
-    const eventBus = instance.appContext.config.globalProperties.emitter
-    const setComponent = (component) => {
-      if(component !== null){
-        console.log('got open setting page event', component)
-        currentIndex.value = component[1]
-        currentComponent.value = {
-          'voltage': VoltageTestItemSettingBox,
-          'current': CurrentTestItemSettingBox,
-          'dmmConfig': LoadTestItemSettingBox,
-          'oscGetWave': OscGetWave,
-          'loadOn': loadOnSettingBox,
-          'loadOff': loadOffSettingBox,
-          'powerSupplyOn': powerSupplyOnSettingBox,
-          'powerSupplyOff': powerSupplyOffSettingBox,
-          'wait': WaitTestItemSettingBox,
-          'serialPort': SerialTestItemSettingBox,
-          'ethernet': EthernetTestItemSettingBox,
-        }[component[0]]
-      }else{
-        currentComponent.value = null
-      }
-    }
-    onMounted(() => {
-      eventBus.on('openSettingPage', setComponent);
-    })
-
-    onUnmounted(() => {
-      eventBus.off('openSettingPage', setComponent);
-    })
-
+  name: 'TestItemSettingBox',
+  data() {
     return {
-      currentComponent,
-      currentIndex
+      show: false,
+      activeTab: 'osc'
     }
   },
-
+  methods: {
+  }
 }
 </script>
 
 <template>
-<div class="z-down-shadow box">
-  <component class="box" :is="currentComponent" :cardIndex="currentIndex"></component>
-</div>
-<div class="submit-button-box">
-  <el-row>
-    <el-col :span="12"></el-col>
-    <el-col :span="12">
-      <el-button class="submit-button z-up-shadow" type="basic White">保存</el-button>
-    </el-col>
-  </el-row>
-</div>
-
+  <div class="box">
+    <el-collapse style="background-color: #f1f2f6">
+      <el-collapse-item title="测试设置">
+        <el-form style="background-color: #f1f2f6">
+          <el-form-item label="测试名称">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="测试描述">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="测试类型">
+            <el-select>
+              <el-option label="自动测试" value="auto"></el-option>
+              <el-option label="手动测试" value="manual"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
 </template>
 
 <style scoped>
 .box {
   margin: 15px;
-  width: 70vh;
-  height: 72vh;
+  background-color: #f1f2f6;
   display: flex;
   flex-direction: column;
   align-items: center;
   border-radius: 10px;
+  box-shadow: inset 10px 10px 20px 10px #ffffff, -2px -2px 1px 1px #ffffff, 4px 4px 6px 4px #d9d9d9;
 
-}
-
-.submit-button {
-  margin-top: 10px;
-  width: 90%;
-  height: 100px;
-  font-size: 30px;
-  font-weight: bold;
-  border-radius: 10px 50px  50px 10px;
-  border: 1px solid #ffffff;
-}
-
-.submit-button:hover {
-  background-color: #ffffff;
-  border: 1px solid #ffffff;
-  font-size: 34px;
-  font-weight: lighter;
-  color: #606266
 }
 
 </style>
