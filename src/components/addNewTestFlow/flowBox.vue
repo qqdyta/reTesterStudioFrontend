36463230adc79bd352b33942588a363f866d5051
qqdyta <qqdyta@163.com>
2024-08-08 14:54:54 +0800

    完成了基础的添加node和edge的 操作

--- src/components/addNewTestFlow/flowBox.vue
+++ src/components/addNewTestFlow/flowBox.vue
@@ -1,38 +1,73 @@
 <script setup>
 /* eslint-disable */
-import { ref, markRaw } from 'vue'
+import {ref, getCurrentInstance, onMounted, markRaw} from 'vue'
 import { VueFlow, useVueFlow } from '@vue-flow/core'
 import { Background } from '@vue-flow/background'
 import { MiniMap } from '@vue-flow/minimap'
 import StartNode from "@/components/addNewTestFlow/flowNodes/startNode.vue"
-const { onInit, onNodeDragStop, onConnect, addEdges } = useVueFlow()
-
+import emitter from "@/main";
+const { onInit, onNodeDragStop, onConnect, addEdges, onNodeClick, addNodes } = useVueFlow()
 
 const dark = ref(false)
 const nodeTypes = {
-  start: StartNode
+  start: markRaw(StartNode)
 }
+
+
+
+
+
 
 onInit((vueFlowInstance) => {
   // instance is the same as the return of `useVueFlow`
   vueFlowInstance.fitView()
 })
 
+
 onNodeDragStop(({ event, nodes, node }) => {
   console.log('Node Drag Stop', { event, nodes, node })
 })
+
+const initialNodes = ref([])
+const initialEdges = ref([])
+onMounted(()=> {
+  emitter.on('addNewTestProgress', (data) => {
+    console.log('addNewTestProgress', data)
+    initialNodes.value = [...initialNodes.value, data]
+    console.log('the node is ', initialNodes.value)
+    addNodes(data)
+    console.log('the data is ', data)
+    if(data.id > 0){
+      initialEdges.value.push({
+        id: ( data.id - 1 ) + '_2_'+data.id,
+        source: (data.id - 1).toString(),
+        target: data.id.toString()
+      })
+      console.log('the edges is ', initialEdges.value)
+    }
+  })
+})
+
+const openSettings = (component) => {
+  emitter.emit('openSettingPage', [component.setType, component.index])
+}
+
 
 const steps = defineProps({
   progress: {
     type: Array,
     default: ()=>{
-      return [{  id: '1',
-        type: 'input',
-        data: { label: 'Node 1' },
-        position: { x: 250, y: 0 }
-      }]
+      return []
     }
   }
+})
+
+const nodes = ref(steps.progress)
+
+onNodeClick((node) => {
+  console.log('Node Click', node.node)
+  console.log('the setting is ', node.node.setType)
+  openSettings({'setType': node.node.setType,'index': node.node.index})
 })
 
 onConnect((connection) => {
@@ -42,16 +77,18 @@ onConnect((connection) => {
 </script>
 <template>
   <VueFlow
-      :nodes="steps.progress"
+      :nodes="initialNodes"
       :class="{ dark }"
+      :edges="initialEdges"
       class="basic-flow"
       :default-viewport="{ zoom: 1.5 }"
       :min-zoom="0.2"
       :max-zoom="4"
       :nodeTypes="nodeTypes"
+      :on-node-click="onNodeClick"
   >
     <Background />
-    <MiniMap />
+    <MiniMap class="mini-map"/>
   </VueFlow>
 </template>
 <style scoped>
@@ -59,6 +96,19 @@ onConnect((connection) => {
 .basic-flow.dark {
   background:#2d3748;
   color:#fffffb
+}
+
+.mini-map{
+  position:absolute;
+  margin: 0px;
+  padding: 0px;
+  right:0;
+  bottom:20px;
+  width:200px;
+  height:100px;
+  border-radius:5px;
+  background:#fffffb
+
 }
 
 .basic-flow.dark .vue-flow__node {
