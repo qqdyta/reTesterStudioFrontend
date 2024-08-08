b0b5642efb386c2f5b97212e177c1ebf624a93d2
qqdyta <qqdyta@163.com>
2024-08-08 17:27:01 +0800

    新增组件

---
+++ src/components/addNewTestFlow/flowNodes/testNode.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="header-box"><HeaderBar></HeaderBar></div>
+  <div class="downer">
+    <div class="left-bar-box"><LeftSideBar></LeftSideBar></div>
+    <div class="main-box"><addNewTestItemBox></addNewTestItemBox></div>
+    <div class="main-box"><addedTestItemBox ></addedTestItemBox></div>
+    <div class="main-box"><TestItemSettingBox></TestItemSettingBox></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
