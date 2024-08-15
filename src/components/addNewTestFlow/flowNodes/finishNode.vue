4c90f89a8faed9d44773c379dd8e4604cf0b582f
qqdyta <qqdyta@163.com>
2024-08-15 16:27:36 +0800

    新增组件

--- src/components/addNewTestFlow/flowNodes/finishNode.vue
+++ src/components/addNewTestFlow/flowNodes/finishNode.vue
@@ -1,11 +1,55 @@
 <script setup>
+/* eslint-disable */
+import { Position, Handle, useVueFlow } from '@vue-flow/core'
+import { computed, inject, defineProps } from 'vue';
+
+// props were passed from the slot using `v-bind="customNodeProps"`
+const props = defineProps(['label'])
+
+const $testItemDict = inject('$testItemDict')
+
+const computedBackgroundColor = computed(() => {
+  return $testItemDict[props.label]['color']
+})
+
+const computedTitle = computed(() => {
+  return $testItemDict[props.label]['title']
+})
+
+//const { onNodeClick } = useVueFlow()
+
+
 
 </script>
 
 <template>
-<div></div>
+  <div class="node-box" :style="{backgroundColor: computedBackgroundColor}">
+    <Handle type="target" class="vue-flow__handle_top" :style="{backgroundColor: computedBackgroundColor}" :position="Position.Top" />
+    <div>{{ computedTitle }}</div>
+  </div>
 </template>
-
 <style scoped>
+
+.node-box{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
+  width: 180px;
+  height: 60px;
+  font-size: 16px;
+}
+
+.vue-flow__handle_top {
+  height:10px;
+  width:15px;
+  border-radius:5px;
+}
+
+.vue-flow__handle_bottom {
+  height:5px;
+  width:15px;
+  border-radius:4px;
+}
 
 </style>
