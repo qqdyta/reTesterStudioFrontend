91eb2b81c9c0132238ab706376f2897769639693
qqdyta <qqdyta@163.com>
2024-08-16 09:48:40 +0800

    继续完善

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,63 +1,74 @@
 <template>
-  <div class="header-box"><HeaderBar></HeaderBar></div>
-  <div class="downer">
-    <div class="left-bar-box"><LeftSideBar></LeftSideBar></div>
-    <div class="main-box"><addNewTestItemBox></addNewTestItemBox></div>
-    <div class="main-box"><addedTestItemBox></addedTestItemBox></div>
-    <div class="main-box"><TestItemSettingBox></TestItemSettingBox></div>
-  </div>
+<div class="main-box">
+  <el-row style="background-color: #f0f0f0">
+    <el-col :span="2"></el-col>
+    <el-col :span="20">
+      <div class="test-plan-table-box">
+        <test-plan-table style="background-color: #f0f0f0" class="test-plan-table" :table-data="tableData"></test-plan-table>
+      </div>
+    </el-col>
+    <el-col :span="2"></el-col>
+  </el-row>
+</div>
+
 
 </template>
 
 <script>
 import "../assets/main.css"
-import HeaderBar from "@/components/HeaderBar.vue"
-import addNewTestItemBox from "@/components/addTestItemComponents/AddNewTestItemBox.vue"
-import addedTestItemBox from "@/components/addTestItemComponents/addedTestItemBox.vue";
-import LeftSideBar from "@/components/LeftSideBar.vue"
-import TestItemSettingBox from "@/components/addTestItemComponents/TestItemSettingBox.vue"
-
+import TestPlanTable from "@/components/homePageComponents/TestPlanTable.vue";
 export default {
   name: ' HomePage',
   components: {
-    TestItemSettingBox,
-    HeaderBar,
-    addNewTestItemBox,
-    LeftSideBar,
-    addedTestItemBox
-  }
+    TestPlanTable
+  },
+  data(){
+    return {
+      component: [],
+      tableData: [
+        { date: '2016-05-02', name: 'John', address: 'New York' },
+        { date: '2016-05-04', name: 'Tom', address: 'Washington, D.C.' },
+        { date: '2016-05-01', name: 'Jerry', address: 'Las Vegas' },
+        { date: '2016-05-03', name: 'Bob', address: 'Los Angeles' }
+      ],
+      counter: 0,
+    }
+  },
+  mounted() {
+    fetch(this.$serverUrl + 'testPlan/', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify({typed: 'getTestPlanList'}), // body data type must match "Content-Type" header
+    }).then(response => response.json())
+        .then(data => {
+          console.log('Success:', data);
+          this.tableData = data.data
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+  },
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-.header-box {
-  width: 100%;
-  height: 5vh;
-  box-shadow: inset 10px 10px 20px 10px #ffffff, -2px -2px 1px 1px #ffffff, 4px 4px 6px 4px #d9d9d9;
-}
-
-.downer {
-  display: flex;
-  flex: 1;
-}
+<style scoped>
 
 .main-box {
   flex: 1;
+  position: relative;
+  flex-direction: column;
+  justify-content: space-between;
   background-color: #f0f0f0;
-  height: 95vh;
-}
+  padding-top: 20px;
 
-.left-bar-box {
-  width: 60px;
-  height:95vh;
-  box-shadow: inset 10px 10px 20px 10px #ffffff, -2px -2px 1px 1px #ffffff, 4px 4px 6px 4px #d9d9d9;
 }
-
 </style>
