10c61cd524b2a3abb0264be6988f9c4b1d7dcb6b
qqdyta <qqdyta@163.com>
2024-08-16 13:47:55 +0800

    修复了mount与unmount的顺序造成的BUG

--- src/components/addTestItemComponents/testItemSettingCard/powerSupplyOnSettingBox.vue
+++ src/components/addTestItemComponents/testItemSettingCard/powerSupplyOnSettingBox.vue
@@ -31,6 +31,23 @@ export default {
         COM: 0
       }
     }
+  },
+  mounted() {
+    console.log('Start beforeMount')
+    this.$emitter.on('getProcessData', (data) => {
+      console.log('Start getProcessData is ', data, this.cardIndex, data.cardIndex, this.cardIndex === data.index)
+      const INDEX = data.cardIndex
+      if(this.cardIndex === INDEX){
+        console.log('the data.data.data is ', data.data.data)
+        this.testItemSettingData = data.data.data
+      }
+    })
+    console.log(' Start mounted')
+    this.$emitter.emit('updateProcessData', {type: 'get', CardIndex: this.cardIndex})
+  },
+  unmounted() {
+    console.log('Start unmounted')
+    this.$emitter.off('getProcessData')
   }
 }
 </script>
