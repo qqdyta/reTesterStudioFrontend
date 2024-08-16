b70d438020d8f6f98f4287b5c3619dca54479860
qqdyta <qqdyta@163.com>
2024-08-16 17:08:53 +0800

    更新组件卡片

---
+++ src/components/testPageComponents/TestingItemDetailBox.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>testingItemDetailBox</div>
+  <el-row>
+    <el-col :span="24">
+    </el-col>
+  </el-row>
+</template>
+
+<style scoped>
+
+</style>
