ebd2dd843714516e16bf1f1c3ce0c9a5874fe3a7
qqdyta <qqdyta@163.com>
2024-08-16 09:48:20 +0800

    新增组件

---
+++ src/components/testPageComponents/TestingItemBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
+    <el-row v-for="i in count" :key="i" class="infinite-list-item">
+      <el-col :span="18">{{ i }}</el-col>
+      <el-col :span="6">成功</el-col>
+    </el-row>
+
+  </ul>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+const load = () => {
+  count.value += 2
+}
+</script>
+
+<style>
+.infinite-list {
+  height: 600px;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+.infinite-list .infinite-list-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  background: var(--el-color-primary-light-9);
+  margin: 10px;
+  color: var(--el-color-primary);
+}
+.infinite-list .infinite-list-item + .list-item {
+  margin-top: 10px;
+}
+</style>
