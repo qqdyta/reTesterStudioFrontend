75e25466f847e295503a8f78f7a56084a6f794b0
qqdyta <qqdyta@163.com>
2024-08-23 16:14:56 +0800

    新增文件上传逻辑

--- src/components/homePageComponents/TestPlanTable.vue
+++ src/components/homePageComponents/TestPlanTable.vue
@@ -61,6 +61,40 @@ const handleDelete = (index, row) => {
       })
 }
 
+const handleExport = (index, row) => {
+  fetch(serverUrl + 'testPlan/', {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify({typed: 'export', id: row.id}), // body data type must match "Content-Type" header
+  }).then(response => {
+    return response.blob()
+  }).then(blob => {
+    const link = document.createElement('a');
+    document.body.appendChild(link);
+    link.style.display = 'none';
+
+    // 设置 URL 和 MIME 类型
+    const url = window.URL.createObjectURL(blob);
+    link.href = url;
+    link.setAttribute('download', 'filename.ext'); // 更改这里的文件名和扩展名
+    // 触发点击
+    link.click();
+
+    // 清理 URL 对象
+    window.URL.revokeObjectURL(url);
+  }).catch((error) => {
+        console.error('Error:', error);
+      })
+}
+
 onMounted(() => {
   console.log('mounted')
 })
@@ -79,6 +113,9 @@ onMounted(() => {
       <template #default="scope">
         <el-button size="small" type="primary" @click="goToTest(scope.$index, scope.row)">
           测试
+        </el-button>
+        <el-button size="small" type="success" @click="handleExport(scope.$index, scope.row)">
+          导出
         </el-button>
         <el-button size="small" @click="handleEdit(scope.$index, scope.row)">
           Edit
