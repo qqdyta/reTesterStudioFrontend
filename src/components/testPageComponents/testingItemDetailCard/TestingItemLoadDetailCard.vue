7de8fed7a0bbd48f4b6a85c214e260bd5ef86171
qqdyta <qqdyta@163.com>
2024-08-23 16:14:34 +0800

    新增测试中信息展示卡片

--- src/components/testPageComponents/testingItemDetailCard/TestingItemLoadDetailCard.vue
+++ src/components/testPageComponents/testingItemDetailCard/TestingItemLoadDetailCard.vue
@@ -1,9 +1,26 @@
 <script setup>
+/* eslint-disable */
+import {defineProps} from 'vue'
+
+const props = defineProps({
+  testPlanId: {
+    type: Number,
+    default: 0
+  },
+  testPlanDetail: {
+    type: Object,
+    default: () => {
+      return {}
+    }
+  }
+})
+
+
 
 </script>
 
 <template>
-<div></div>
+<div>LoadOne</div>
 </template>
 
 <style scoped>
