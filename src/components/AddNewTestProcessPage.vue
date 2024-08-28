4166c20d805caee5ffcbba1f90ab9ce3e7215c9a
qqdyta <qqdyta@163.com>
2024-08-28 19:24:51 +0800

    新增了开始和结束节点的判断

--- src/components/AddNewTestProcessPage.vue
+++ src/components/AddNewTestProcessPage.vue
@@ -15,6 +15,7 @@ import addNewTestItemBox from "@/components/addTestItemComponents/AddNewTestItem
 import flowBox from "@/components/addNewTestFlow/flowBox.vue";
 import TestItemSettingBox from "@/components/addTestItemComponents/TestItemSettingBox.vue"
 import { getCurrentInstance } from "vue";
+import {ElNotification} from "element-plus";
 export default {
   name: 'App',
   components: {
@@ -51,20 +52,27 @@ export default {
       }else if(data == 'onEnd'){
         nodeType = 'finish'
       }
-
-      const TEST_DATA = {
-        id: this.counter++,
-        setType: data,
-        label: data,
-        index: this.counter,
-        position: {x: this.xPosition , y: this.yPosition},
-        data: {label: 'TestNode' + this.counter},
-        class: 'light',
-        type: nodeType
+      if(this.counter == 0 && data !== 'onStart'){
+        ElNotification({
+          title: '第一个节点必须是开始节点',
+          message: '开始节点位于 其他 选项卡中',
+          type: 'warning',
+        })
+      }else{
+        const TEST_DATA = {
+          id: this.counter++,
+          setType: data,
+          label: data,
+          index: this.counter,
+          position: {x: this.xPosition , y: this.yPosition},
+          data: {label: 'TestNode' + this.counter},
+          class: 'light',
+          type: nodeType
+        }
+        this.$emitter.emit('addNewTestProgress', TEST_DATA)
+        this.yPosition += 100
+        this.component.push(TEST_DATA)
       }
-      this.$emitter.emit('addNewTestProgress', TEST_DATA)
-      this.yPosition += 100
-      this.component.push(TEST_DATA)
     },
     openSettingPage(component) {
       this.$emit('openSettingPage', component)
