06cacc72232932752bf8866c4b164fb01b505396
qqdyta <qqdyta@163.com>
2024-08-29 17:14:03 +0800

    实现了对既有测试步骤的编辑与保存

--- src/components/homePageComponents/TestPlanTable.vue
+++ src/components/homePageComponents/TestPlanTable.vue
@@ -32,6 +32,31 @@ const goToTest = (index, row) => {
 
 const handleEdit = (index, row) => {
   console.log(index, row)
+  fetch(proxy.$serverUrl + 'testPlan/', {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify({typed: 'getSingleTestPlan', id: row.id}), // body data type must match "Content-Type" header
+  }).then(response => response.json())
+      .then(data => {
+        const RESULT = data.data[0]
+        console.log('Success:', RESULT);
+        proxy.$emitter.emit('settingRightBox', ['add'])
+        setTimeout(()=>{
+          proxy.$emitter.emit('loadTestStep', {'testStep': RESULT.testStep, 'edges': RESULT.testEdge, id: RESULT.id })
+        }, 200)
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      })
+
 }
 
 const handleDelete = (index, row) => {
